bdf9ba023afdfaf11328e314bff9ed2ad04fdc43
xavierace990 <xavierparzival@gmail.com>
2024-03-28 01:17:54 -0700

    added files via upload

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to='uploads')
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
---
+++ project/run_server.py
@@ -0,0 +1,6 @@
+# run_server.py
+
+from django.core.wsgi import get_wsgi_application
+
+# Get the WSGI application object for your Django project
+application = get_wsgi_application()
